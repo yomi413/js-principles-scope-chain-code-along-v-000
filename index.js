d8ab8e232441bb717e6505e2c107b5f34f47fb13
Yomaira Escano <yjescano@gmail.com>
2018-07-25 04:39:42 +0000

    Done.

--- index.js
+++ index.js
@@ -1,28 +1,69 @@
-var customerName = 'bob'
-const leastFavoriteCustomer = 'sour fred'
+var customerName = 'bob';
+const leastFavoriteCustomer = 'yelling gordon'
 
 function upperCaseCustomerName() {
-  return customerName = customerName.toUpperCase()
+  return customerName = customerName.toUpperCase();
 }
 
-function changeLeastFavoriteCustomer(){
-  leastFavoriteCustomer = 'sour fred'
+function setBestCustomer() {
+  bestCustomer = 'not bob';
+}
+
+function overWriteBestCustomer() {
+  bestCustomer = 'maybe bob';
+}
+
+function changeLeastFavoriteCustomer() {
+  leastFavoriteCustomer = 'nagging nellie';
+}
+
+function attemptTwoFavoriteCustomers() {
+  let favoriteCustomer = 'sally';
+  let favoriteCustomer = 'charlie';
 }
 
 
-function setBestCustomer(){
-  bestCustomer = 'not bob'
-}
 
-function overWriteBestCustomer(newFavorite){
-  bestCustomer = newFavorite
-}
 
-function congratulateCustomer(){
-  return `congrats ${favoriteCustomer}`
-}
 
-function attemptTwoFavoriteCustomers(){
-    let favoriteCustomer = 'bar'
-    let favoriteCustomer = 'not bar'
-}
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var customerName = 'bob'
+// const leastFavoriteCustomer = 'sour fred'
+//
+// function upperCaseCustomerName() {
+//   return customerName = customerName.toUpperCase()
+// }
+//
+// function changeLeastFavoriteCustomer(){
+//   leastFavoriteCustomer = 'sour fred'
+// }
+//
+//
+// function setBestCustomer(){
+//   bestCustomer = 'not bob'
+// }
+//
+// function overWriteBestCustomer(newFavorite){
+//   bestCustomer = newFavorite
+// }
+//
+// function congratulateCustomer(){
+//   return `congrats ${favoriteCustomer}`
+// }
+//
+// function attemptTwoFavoriteCustomers(){
+//     let favoriteCustomer = 'bar'
+//     let favoriteCustomer = 'not bar'
+// }
